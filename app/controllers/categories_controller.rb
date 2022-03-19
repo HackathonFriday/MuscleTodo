4cc9a68503ec79217f8725fcbee551b431ad4c8e
r.saito <r.saito@core-tech.jp>
2022-03-19 20:17:11 +0900

    implement categorey insert logic

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,12 @@
 class CategoriesController < ApplicationController
   def create
+    Category.create(category_params)
+
+    redirect_to mypage_show_path
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name, :user_id)
   end
 end
