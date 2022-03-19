143b2a926cb811d2202398dd6d99f8e6093d404c
s.yokomizo <>
2022-03-19 18:53:35 +0900

    キャラクターとセリフをモーダルへ表示

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,7 +1,7 @@
 class MypageController < ApplicationController
   def show
 
-    is_creation_flg = true; # テスト用にタスク作成時と完了時のフラグをここで設定
+    is_creation_flg = false; # テスト用にタスク作成時と完了時のフラグをここで設定
 
     characters = Character.pluck(:image_path)
     phrases = Phrase.where(is_creation: is_creation_flg).pluck(:content)
