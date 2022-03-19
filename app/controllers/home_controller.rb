98a329daa2f701365b7b6f2871dcaf7d114d857b
KazuhiroTakahashi <k_takahashi@just-ltd.co.jp>
2022-03-19 18:18:15 +0900

    ユーザー登録機能実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
   def index
+    @user = User.new()
   end
+
+	def create
+		@new_user = User.create(user_params)
+		redirect_to mypage_show_path
+	end
+
+	private
+		def user_params
+			params.require(:user).permit(:name, :gender, :token)
+		end
 end
