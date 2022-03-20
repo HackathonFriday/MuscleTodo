6296ed71099b298103de1ede7a94965468473cf8
r.saito <r.saito@core-tech.jp>
2022-03-20 11:12:26 +0900

    一部修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create([
-    { name: '出藻 戻夫', gender: 1, token: 'z9sAhRvH'},
+    { name: '出藻 戻夫', gender: 1, token: 'z9sAhRvH', exp: 180},
     { name: 'かーしー', gender: 1, token: 'uEnKRSX5'},
     { name: 'しょーき', gender: 1, token: 'ksTXZ9WM'},
     { name: 'りょーた', gender: 1, token: 'akfd9RvH'},
